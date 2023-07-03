4df1d9f9e7839f78fb0ff60834c22e7ff2637465
Trieu Nguyen <44500850+ngqtrieu99@users.noreply.github.com>
2023-07-03 20:00:03 +0700

    Init the Identity class (#34)
    Co-authored-by: Sam Nguyen <Sam.Nguyen@avepoint.com>

--- MoneyManager/AppServer/Controllers/GoogleServiceController.cs
+++ MoneyManager/AppServer/Controllers/GoogleServiceController.cs
@@ -8,22 +8,18 @@ namespace AppServer.Controllers
     [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
         }
-
+        
         [Authorize]
         [HttpGet("information")]
         public async Task<IActionResult> GetInformation()
         {
             return Ok("Get Successfully");
         }
-
-
     }
 }
---
+++ MoneyManager/AppServer/RequestModels/SignInRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppServer.RequestModels
+{
+    public class SignInRequest
+    {
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
